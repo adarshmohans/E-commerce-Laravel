610e14c4e8867d51b18848f359fc24121292cba6
Adarsh Mohan S <125942633+adarshmohans@users.noreply.github.com>
2025-02-09 04:44:44 +0530

    Footer

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -203,13 +203,7 @@
     <!-- /.control-sidebar -->
 
     <!-- Main Footer -->
-    <footer class="main-footer">
-        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">Ekart</a>.</strong>
-        All rights reserved.
-        <div class="float-right d-none d-sm-inline-block">
-            <b>Version</b> 3.2.0
-        </div>
-    </footer>
+
     </div>
     <!-- ./wrapper -->
 
